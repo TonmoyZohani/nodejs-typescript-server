52e49a66c59810832c2730b54d44ff3932bf10ef
TonmoyZohani <tonmoyzohani@gmail.com>
2025-12-01 15:02:00 +0600

    post creation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import parseBody from "../helpers/parseBody";
 import addRoutes from "../helpers/RouteHandler";
 import sendJson from "../helpers/sendJson";
 
@@ -7,4 +8,9 @@ addRoutes("GET", "/", (req, res) => {
 
 addRoutes("GET", "/api", (req, res) => {
   sendJson(res, 200, { message: "Health Status Ok", path: req.url });
+});
+
+addRoutes("POST", "/api/users", async (req, res) => {
+  const body = await parseBody(req);
+  sendJson(res, 200, { success: true, data: body, message: "User Created" });
 });
